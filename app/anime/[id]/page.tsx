819bede7b6365e1d49c8d6cf4ecb6cb7ce174784
rimo02 <ghoshrimo02@gmail.com>
2025-05-12 02:18:56 +0530

    fixed build production issues

--- app/anime/[id]/page.tsx
+++ app/anime/[id]/page.tsx
@@ -3,8 +3,12 @@ import { getAnimeById } from '@/lib/api';
 
 export const revalidate = 3600;
 
-export default async function AnimePage({ params }: { params: { id: string } }) {
-    const id = Number((await params).id);
+type PageProps = {
+    params: { id: string };
+};
+
+export default async function AnimePage({ params }: PageProps) {
+    const id = Number((params).id);
     const response = await getAnimeById(id);
 
     const anime = response.data;
